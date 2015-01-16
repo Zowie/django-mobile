ee808277d8e1baef2f854c07be077d8ccb2c1793
Zowie <zowielangdon@gmail.com>
2015-01-16 09:55:38 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def get_version(package):
     """
     Return package version as listed in `__version__` in `init.py`.
     """
-    init_py = open(os.path.join(package, '__init__.py')).read().decode('utf-8')
+    init_py = open(os.path.join(package, '__init__.py'), encoding='utf-8').read()
     return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
 
 
