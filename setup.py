705ba2cf91ca7a631b4d53013c5840d534f45254
Zowie <zowielangdon@gmail.com>
2015-01-16 09:54:28 +0100

    Fix encoding when installing with pip3

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def get_version(package):
     """
     Return package version as listed in `__version__` in `init.py`.
     """
-    init_py = open(os.path.join(package, '__init__.py')).read()
+    init_py = open(os.path.join(package, '__init__.py')).read().decode('utf-8')
     return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
 
 
